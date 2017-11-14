363743ca994b03ad26e4a90d5d35cd6cb6651c2b
Alex Wildman <Alex>
2017-11-14 10:16:59 -0700

    got a request from google back

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+var mainVm = new Vue({
+	// google.maps.event.addDomListener(window, 'load', init);
+
+    el: '#app',
+    data: {
+		userinput:'',
+    },
+	methods: {
+		submitlocation: function() {
+			var userinput = this.userinput
+			console.log(userinput)
+			// $.get(`/search?query=${userinput}`, function(dataFromServer){
+			// 	console.log(dataFromServer)
+			// })
+			$.get(`/search`, userinput, function(dataFromServer){
+				console.log(dataFromServer)
+			})
+
+			// var vm = this;
+
+			// axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + this.userinput + '&key=AIzaSyBQaVXoS5ADQRsIvPFGug1J2To4HPUk84I')
+			// .then(function(datareturned){
+			// 	console.log(datareturned)
+			// })
+			// // this.$http.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + this.userinput + '&key=AIzaSyBQaVXoS5ADQRsIvPFGug1J2To4HPUk84I', function(data) {
+			// // 	console.log('sent successfully')
+			// // 	console.log(data)
+			// // })
+		}
+	}
+})
